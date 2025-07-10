b9754a9c42186e3161f9de4944eb67121028193c
meenaskhi624 <m.nahate24@gmail.com>
2025-07-10 11:26:09 +0530

    first commit

---
+++ src/redux/reducer/cartSlice.js
@@ -0,0 +1,31 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  product: [],
+};
+
+const cartSlice = createSlice({
+  name: "cart",
+  initialState,
+  reducers: {
+    // add(state, action) {
+    //     console.log("data Added")
+    // },
+    // add(state,action) {
+    //     state.product.push(action.payload);
+    // },
+    add(state,action) {
+        const alreadyExist= state.product.find((item)=>item.id === action.payload.id)
+        if(! alreadyExist){
+          state.product.push(action.payload);
+        }      
+    },
+    remove(state, action) {
+       const updatedProducts= state.product.filter((item)=> item.id != action.payload);
+       state.product=updatedProducts;
+    },
+  },
+});
+
+export const { add, remove } = cartSlice.actions;
+export default cartSlice.reducer;
